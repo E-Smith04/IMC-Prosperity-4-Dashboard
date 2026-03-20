102199ffe26d2cd42f73b85866ca9acc0c24bd66
Edwin Smith <edwin.smith.es40@gmail.com>
2026-03-20 23:28:05 +0000

    Added schemas (#10)

---
+++ libs/data-platform-py/src/data_platform_sdk/schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from enum import Enum
+
+
+class Product(str, Enum):
+    EMERALDS = "EMERALDS"
+    TOMATOES = "TOMATOES"
+
+
+
+class Traders(str, Enum):
+    pass
+
+
+class FiltersBase(BaseModel):
+    day: int | None = None
+    timestamp_min: int | None = None
+    timestamp_max: int | None = None
+
+
+class PriceFilters(FiltersBase):
+    product: Product | None = None
+
+
+class TradeFilters(FiltersBase):
+    symbol: Product | None = None
+    quantity_min: int | None = None
+    quantity_max: int | None = None
+    buyers: list[Traders] | None = None
+    sellers: list[Traders] | None = None
