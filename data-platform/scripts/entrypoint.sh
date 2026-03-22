101a650b8af57b04dd21e8954dba883786b646be
Edwin Smith <edwin.smith.es40@gmail.com>
2026-03-22 19:25:02 +0000

    Added pipelines (#25)

--- data-platform/scripts/entrypoint.sh
+++ data-platform/scripts/entrypoint.sh
@@ -6,7 +6,15 @@ echo "Running pipelines..."
 
 spark-pipelines run \
     --remote "$SPARK_CONNECT_URL" \
-    --spec "src/data_platform/pipelines/imc_prosperity_pipeline/spark-pipeline.yml"
+    --spec "src/data_platform/pipelines/bronze_pipeline/spark-pipeline.yml"
+
+spark-pipelines run \
+    --remote "$SPARK_CONNECT_URL" \
+    --spec "src/data_platform/pipelines/silver_pipeline/spark-pipeline.yml"
+
+spark-pipelines run \
+    --remote "$SPARK_CONNECT_URL" \
+    --spec "src/data_platform/pipelines/gold_pipeline/spark-pipeline.yml"
 
 echo "Pipelines complete. Starting API..."
 
